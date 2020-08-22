253fa2b2b3c85ea29b84cffe870f31187c460024
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-08-22 21:15:17 +0200

    added home app in templates

--- best_brand/settings.py
+++ best_brand/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,10 @@ ROOT_URLCONF = 'best_brand.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
