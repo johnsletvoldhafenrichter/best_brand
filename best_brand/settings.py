a6a38aa158a4afe143c2a926c09812c32b9181c0
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-08-24 01:57:35 +0200

    Completed confirmation emails

--- best_brand/settings.py
+++ best_brand/settings.py
@@ -184,3 +184,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY', default=os.environ.get('STRIPE_PUBLIC_KEY', ''))
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY', default=os.environ.get('STRIPE_SECRET_KEY', ''))
 STRIPE_WH_SECRET = env('STRIPE_WH_SECRET', default=os.environ.get('STRIPE_WH_SECRET', ''))
+DEFAULT_FROM_EMAIL = 'bestbrand@example.com'
