b30b88d9ac8e77897a06bca2b0718c8df064a75f
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-08-23 18:43:21 +0200

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HJLu4LfjCgk81oTXf3dqu1TbPL2LsbXvOR1cC8yP5ZHHeooZlH0xAIsEBVRhSffNnvAHObX60BOQsZYjXzNsodt000XuZuiDw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
