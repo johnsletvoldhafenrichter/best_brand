523a7cac6cb3bf6a7782599a3913deaf94e6911a
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-08-24 17:47:22 +0200

    changed hostname

--- best_brand/settings.py
+++ best_brand/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY', default=os.environ.get('SECRET_KEY', ''))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://best-brand.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['best-brand.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
